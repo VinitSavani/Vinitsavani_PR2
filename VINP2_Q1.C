8432e93485b701d8dd5e6aa4cd62eda740691444
vinit <vinitsavani001@gmail.com>
2023-06-27 17:28:30 +0530

    first"v

---
+++ VINP2_Q1.C
@@ -0,0 +1,16 @@
+// PR2-Q1 write a programe to check whether a number is even or odd using the ternary operator.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int A;
+clrscr();
+printf("-*-*-*-*-*-*-*-*-*-\n");
+printf("Enter any number :");
+scanf("%d",&A);
+printf("-*-*-*-*--*-*-*-*-*-\n");
+(A % 2 == 0)?printf("%d is Even number\n",A)
+	    :printf("%d is Odd number\n",A);
+getch();
+}
---
+++ VINP2_Q2.C
@@ -0,0 +1,19 @@
+// PR2_Q2 Write a programe to input electricity unit charges and calculate the totle electricity bill accordingly to the given condition.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int unit;
+float amount,total_amount,sarcharge;
+clrscr();
+printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+printf("Total consumption of Electricity in units:");
+scanf("%d",&unit);
+printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+if (unit<=50){
+
+}
+
+getch();
+}
